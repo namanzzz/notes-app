d371d406778732f020b72206a185828a4984709b
naman <namansharma50962@gmail.com>
2024-07-07 18:47:49 +0530

    auth route

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const expressLayouts = require('express-ejs-layouts');
 const connectDB = require('./server/config/db');
 // import express from 'express'
 // import expressLayouts from 'express-ejs-layouts'
+const session = require('express-session');
+const passport = require('passport');
+const MongoStore = require('connect-mongo');
 
 require('dotenv').config();
 
 
 const app = express();
 const port = 5000 || process.env.PORT;
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -28,6 +37,7 @@ app.set('layout', './layouts/main');
 app.set('view engine','ejs');
 
 // Routes
+app.use('/', require('./server/routes/auth'));
 app.use('/',require('./server/routes/index'));
 app.use('/',require('./server/routes/dashboard'));
 
