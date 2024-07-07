445b207699a24032e2446456525e39f373c9eaa0
naman <namansharma50962@gmail.com>
2024-07-07 19:39:56 +0530

    authentication

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    /* */
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+router.get('/google',
+    passport.authenticate('google', { scope: ['profile'] }));
+  
+
+router.get('/google/callback', 
+    passport.authenticate('google', { 
+        failureRedirect: '/login-failure',
+        successRedirect: '/dashboard'
+     }),
+);
+
+
+// login failure router
+router.get('/login-failure', (req,res)=>{
+    res.send('Something Went Wrong!')
+})
+
 
 
 
